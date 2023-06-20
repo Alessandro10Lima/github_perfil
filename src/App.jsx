7deafcf7b1965e232bfe5489b72de5a150274d59
alessanrolima0292@gmail.com <alessandrolima0292@gmail.com>
2023-06-19 23:51:00 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import Perfil from "./components/Perfil"
+import Formulario from "./components/Formulario"
+import ReposList from "./components/ReposList";
+
+function App() {
+  const [formularioVisivel, setFormularioVisivel] = useState(true);
+  const [nomeUsuario, setNumeroUsuario] = useState('');
+
+  return (
+    <>
+      <input type="text" onBlur={(e) => setNumeroUsuario(e.target.value)} />
+
+    {nomeUsuario.length > 4 && (
+      <>
+        <Perfil nomeUsuario={nomeUsuario}/>
+        <ReposList nomeUsuario = {nomeUsuario}/>
+      </>
+    )}
+    
+    {/* {formularioVisivel && (
+      <Formulario />
+    )}
+    <button type="button" onClick={() => setFormularioVisivel(!formularioVisivel)}>toggle form</button> */}
+    </>
+  )
+}
+
+export default App
